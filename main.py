1d7661c52fd043fd264c2e21bcb5ba1660774f38
김현겸 <gimhyeongyeom@gimhyeongyeoms-MacBook-Air.local>
2022-01-14 08:50:24 +0900

    final

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import string
+from turtle import title
 from typing import overload
 from PyQt5.QtGui import QIcon, QStandardItemModel
 from PyQt5.uic.uiparser import QtCore, QtWidgets
@@ -28,13 +30,14 @@ def resource_path(relative_path):
 form = resource_path('panda_qt.ui')
 form_class = uic.loadUiType(form)[0]
 
-def render_mpl_table(data, col_width=3.0, row_height=0.625, font_size=14,
+def render_mpl_table(data, title, col_width=3.0, row_height=0.625, font_size=11,
                      header_color='#717ED1', row_colors=['#FFFFFF', 'w'], edge_color='#000000',
-                     bbox=[0, 0, 1, 1], header_columns=0,
+                     bbox=[0, 0, 1, 1], header_columns=0, 
                      ax=None, **kwargs):
     if ax is None:
         fig, ax = plt.subplots(figsize=(8.27, 11.69))
         ax.axis('off')
+        ax.set_title(title ,pad=20)
     mpl_table = ax.table(cellText=data.values, cellLoc='center', bbox=bbox, colLabels=data.columns, **kwargs)
     mpl_table.auto_set_font_size(False)
     mpl_table.set_fontsize(font_size)
@@ -86,10 +89,10 @@ class Data():
     def calcScore(self):
         for i in range(len(self.__df.index)):
             correct = self.__df.iloc[i, 1]
-            if correct != -1:
-                score = correct / self.__maxCor
-                score2 = round(score, 2) * 100
-                self.__df.iloc[i, 2] = score2
+            if correct != -1 and correct != '미응시':
+                score = int(correct) / self.__maxCor
+                score2 = score * 100
+                self.__df.iloc[i, 2] = round(score2, 1)
             else:
                 self.__df.iloc[i, 1] = '미응시'
                 self.__df.iloc[i, 3] = 0
@@ -99,7 +102,7 @@ class Data():
         grade = 1
         for i in range(len(self.__df.index)):
             if self.__df.iloc[i, 1] != '미응시':
-                self.__df.iloc[i, 3] = grade
+                self.__df.iloc[i, 3] = round(grade, 0)
             else:
                 self.__df.iloc[i, 3] = 0
             grade += 1
@@ -147,11 +150,14 @@ class Data():
         self.__df = x
 
     def hideName(self):
-        scoreList = self.__df.점수
+        scoreList = self.__df.점수.tolist()
+        print(scoreList)
 
         for i in range(len(scoreList)):
-            if scoreList[i] < 80:
+            print(scoreList[i])
+            if float(scoreList[i]) < 80.0:
                 name = self.__df.iloc[i, 0]
+                print(name)
                 newName = name[0] + '**'
                 self.__df.iloc[i, 0] = newName
 
@@ -310,39 +316,6 @@ class WindowClass(QMainWindow, form_class) :
         else:
             QMessageBox.about(self, '경고', '파일을 찾을 수 없습니다.')
 
-    def updateItemToList(self):
-        pandaList = self.listWidget
-        pandaGrade = self.comboBox.currentText()
-        pandaDate = self.dateEdit.date().toString(Qt.ISODate)
-        text = self.titleEdit.text()
-        item = pandaList.currentItem().text()
-        cur_row = pandaList.currentRow()
-        itemText = f'{pandaGrade} {pandaDate} 성적표' if text == '' else text
-        file = f'{pandaGrade}.xlsx'
-
-        if os.path.isfile(file):
-            book = load_workbook(file)
-
-            if item in book.sheetnames:
-                if len(book.sheetnames) < 2:
-                    book1 = book.create_sheet()
-                    book1.title = '~~~'
-                    book1['A1'] = '~~~'
-
-                    book.remove(book[item])
-                    data.saveToExcel(pandaGrade, itemText)
-
-                    book.remove(book['~~~'])
-                    book.save(file)
-                else:
-                    book.remove(book[item])
-                    book.save(file)
-
-                    data.saveToExcel(pandaGrade, itemText)
-
-                pandaList.takeItem(cur_row)
-                pandaList.insertItem(cur_row, itemText)
-
     def delItemToList(self):
         pandaList = self.listWidget
         cur_row = pandaList.currentRow()
@@ -352,6 +325,9 @@ class WindowClass(QMainWindow, form_class) :
 
         if os.path.isfile(file):
             book = load_workbook(file)
+            if len(book.sheetnames) <= 1:
+                QMessageBox.about(self, '경고', '하나 이상의 시트가 있어야 합니다.')
+                return
             if text in book.sheetnames:
                 book.remove(book[text])
                 book.save(file)
@@ -411,7 +387,7 @@ class WindowClass(QMainWindow, form_class) :
         
         r_list = data.getDf().이름
         c_list = ['맞은 갯수', '점수', '순위']
-
+        
         table.clear()
         table.setHorizontalHeaderLabels(c_list)
         table.setVerticalHeaderLabels(r_list)
@@ -464,41 +440,65 @@ class WindowClass(QMainWindow, form_class) :
 
 
     def clearTable(self):
+        self.tableWidget.clear()
         self.tableWidget.setRowCount(0)
+        col = data.getDf().columns
+
+        self.tableWidget.setHorizontalHeaderLabels(col)
 
     def printSheetSignal(self):
         df = copy.deepcopy(data.getDf())
+        mis_list = []
 
         if len(df.index) < 1:
             QMessageBox.about(self, '경고', '테이블이 없습니다.')
-            return
-
-        if len(df.index) <= 30:
-            leng = len(df.index)
-            for i in range(leng, 31):
-                df = df.append({'순위': '', '이름': '', '점수': ''}, ignore_index=True)
-        else:
             return
 
         if '맞은 갯수' in df.columns:
             df.drop('맞은 갯수', axis=1, inplace=True)
             col_change = ['순위', '이름', '점수']
             df = df.reindex(columns=col_change)
-        
+
+        if len(df.index) <= 30:
+            leng = len(df.index)
+            missing = 0
+            for i in range(leng):
+                if df.iloc[i, 0] == 0:
+                    missing += 1
+                    mis_list.append(df.iloc[i, 1])
+                    df = df.drop(df.index[i])
+                    print('missing found?!')
+            col_len = 30 - leng + missing
+            for i in range(col_len):
+                df = df.append({'순위': '', '이름': '', '점수': ''}, ignore_index=True)
+        else:
+            return
+        print(mis_list)
         print(df)
         
-        for i in range(len(df.index)):
-            if df.iloc[i, 0] == 0:
-                df.iloc[i, 0] = '미응시'
-                df.iloc[i, 2] = ''
+        d = {"순위": [], "이름": [], "점수": []}
+        dfm = pd.DataFrame(data=d)
+        for i in range(len(mis_list)):
+            dfm = dfm.append({'순위': '미응시', '이름': mis_list[i], '점수': ''}, ignore_index=True)
+        dfm.sort_values(by=['이름'], axis=0, ascending=True, inplace=True)
+        print(dfm)
+        
+        df = pd.concat([df, dfm], axis=0, ignore_index=True)
+        print(df)
 
         if df.iloc[-1, 0] != '평균':
             avg = data.calcAvg()
             avgStr = f'{avg:.2f}'
             df = df.append({'순위': '평균', '이름': '', '점수': avgStr}, ignore_index=True)
         
-        fig,ax = render_mpl_table(df, header_columns=0, col_width=2.0)
-        fig.savefig('table.pdf')
+        if self.titleEdit.text() != '':
+            title = self.titleEdit.text()
+        else:
+            title = self.listWidget.currentItem().text()
+
+        fig,ax = render_mpl_table(df, title, header_columns=0, col_width=2.0)
+        fig.savefig(f'{title}.pdf')
+        
                 
 
 if __name__ == "__main__" :
